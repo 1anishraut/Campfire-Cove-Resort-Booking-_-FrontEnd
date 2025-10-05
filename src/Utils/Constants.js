92e9553e273e2f8a57cb975844dd594b99e923a2
Anish Raut <anishraut.555@gmail.com>
2025-10-06 01:30:22 +0530

    s

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -3,8 +3,7 @@
 //     ? "http://localhost:3000"
 //     : "https://campfire-cove-resort-booking-back-e.vercel.app/";
 
-export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+export const BASE_URL ="/api";
 
 
 // export const BASE_URL = "https://campfire-cove-resort-booking-back-e.vercel.app";
